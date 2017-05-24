b2e8e74cc55da6f81a5c3190c6695c08a2447325
Jeremy Fillingim <jeremy@passive-logic.com>
2017-05-23 22:11:36 -0600

    Added USB serial interface and ability to write commands via USB.
    Automatically read up to 255 characters with a timeout of 1 second.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 '''
 '''
 
+import serial
+
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
 
@@ -27,20 +29,15 @@ def main(args):
     server = SimpleXMLRPCServer(("localhost", 8000), requestHandler=RequestHandler)
     server.register_introspection_functions()
 
-    # Register pow() function; this will use the value of
-    # pow.__name__ as the name, which is just 'pow'.
-    server.register_function(pow)
-
-    # Register a function under a different name
-    def adder_function(x,y):
-        return x + y
-    server.register_function(adder_function, 'add')
-
-    # Register an instance; all the methods of the instance are
-    # published as XML-RPC methods (in this case, just 'div').
     class MyFuncs:
-        def div(self, x, y):
-            return x // y
+        def __init__(self):
+            self.ser = serial.Serial('/dev/ttyACM1', timeout=1)
+
+        def write(self, cmd):
+            print(cmd)
+            self.ser.write(b'{}\r\n'.format(cmd))
+            self.ser.flush()
+            return self.ser.read(255)
 
     server.register_instance(MyFuncs())
 
