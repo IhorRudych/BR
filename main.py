21d2fe34e4d03cbea8f36d6ed2a3135ca4927d47
Jeremy Fillingim <jfilling@gmail.com>
2018-10-18 13:41:45 -0600

    Disable log-to-file in Bridge by default.

--- main.py
+++ main.py
@@ -64,16 +64,18 @@ def main(args):
     logger = logging.getLogger('xmlrpc')
     logger.setLevel(logging.ERROR)
 
-    # create a file handler
-    handler = logging.FileHandler('xmlrpc.log')
-    handler.setLevel(logging.INFO)
+    LOG_TO_FILE = False
+    if LOG_TO_FILE:
+        # create a file handler
+        handler = logging.FileHandler('xmlrpc.log')
+        handler.setLevel(logging.INFO)
 
-    # create a logging format
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
+        # create a logging format
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
 
-    # add the handlers to the logger
-    logger.addHandler(handler)
+        # add the handlers to the logger
+        logger.addHandler(handler)
 
     # Create server
     server = SimpleXMLRPCServer((args.host, args.port), requestHandler=RequestHandler)
